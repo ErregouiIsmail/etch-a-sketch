fcc5cee7677f8387b7e847607a9992fc31b35620
Ismail <erregouiismail@gmail.com>
2024-11-13 20:02:55 +0100

    Finished the HTML and CSS code

---
+++ script.js
@@ -0,0 +1,31 @@
+// const parentDiv = document.querySelector(".parent");
+// const button = document.querySelector("button");
+// const inputs = document.querySelectorAll("input");
+
+// button.addEventListener("click", () => {
+//   const promptValue = prompt("Chouse your grid size between 1 and 100.", 16);
+//     const size = number()
+//   inputs.forEach((input) => {
+//     input.value = promptValue;
+//     button.textContent = "Reset the gride size.";
+//   });
+//   console.log(promptValue);
+
+//     const inputValue = parseInt(input.value);
+//     const gridSize = Math.pow(inputValue, 2);
+//     console.log(inputValue);
+//     console.log(gridSize);
+//     const hightAndWidth = 700 / inputValue;
+//     for (let i = 0; i < gridSize; i++) {
+//       const childDiv = document.createElement("div");
+//       // child.classList.toggle("child");
+//       // childDiv.style.backgroundColor = " rgb(182, 21, 21)";
+//       childDiv.style.boxSizing = "border-box";
+//       childDiv.style.border = "1px solid black";
+//       childDiv.style.height = hightAndWidth + "px";
+//       childDiv.style.width = hightAndWidth + "px";
+
+//       // console.log(childDiv.style.height.value);
+//       parentDiv.appendChild(childDiv);
+//    }
+// });
