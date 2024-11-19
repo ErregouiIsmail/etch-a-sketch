21a0a8ffa065c1237ff5e1a8d6586ade8b095674
Ismail <erregouiismail@gmail.com>
2024-11-19 19:02:53 +0100

    created js function for the basic use of the project

--- script.js
+++ script.js
@@ -1,31 +1,62 @@
-// const parentDiv = document.querySelector(".parent");
-// const button = document.querySelector("button");
-// const inputs = document.querySelectorAll("input");
+const parentDiv = document.querySelector(".parent");
+const button = document.querySelector("button");
+const inputs = document.querySelectorAll("input");
+let reset = false;
 
-// button.addEventListener("click", () => {
-//   const promptValue = prompt("Chouse your grid size between 1 and 100.", 16);
-//     const size = number()
-//   inputs.forEach((input) => {
-//     input.value = promptValue;
-//     button.textContent = "Reset the gride size.";
-//   });
-//   console.log(promptValue);
+function inputPrompt() {
+  let valide = false;
+  while (valide === false) {
+    const promptValue = prompt("Chouse your grid size between 1 and 100.", 16);
+    if (promptValue == null) {
+      break;
+    }
 
-//     const inputValue = parseInt(input.value);
-//     const gridSize = Math.pow(inputValue, 2);
-//     console.log(inputValue);
-//     console.log(gridSize);
-//     const hightAndWidth = 700 / inputValue;
-//     for (let i = 0; i < gridSize; i++) {
-//       const childDiv = document.createElement("div");
-//       // child.classList.toggle("child");
-//       // childDiv.style.backgroundColor = " rgb(182, 21, 21)";
-//       childDiv.style.boxSizing = "border-box";
-//       childDiv.style.border = "1px solid black";
-//       childDiv.style.height = hightAndWidth + "px";
-//       childDiv.style.width = hightAndWidth + "px";
+    const inputValue = parseInt(promptValue);
 
-//       // console.log(childDiv.style.height.value);
-//       parentDiv.appendChild(childDiv);
-//    }
-// });
+    if (!isNaN(inputValue) && inputValue > 0 && inputValue <= 100) {
+      valide = true;
+      return inputValue;
+    }
+  }
+}
+
+function createGrid(inputValue) {
+  if (inputValue != null) {
+    inputs.forEach((input) => {
+      input.value = inputValue;
+      button.textContent = "Reset the gride.";
+    });
+    gridSize = Math.pow(inputValue, 2);
+    const hightAndWidth = 700 / inputValue;
+    for (let i = 0; i < gridSize; i++) {
+      let childDiv = document.createElement("div");
+      childDiv.classList.toggle("child");
+      childDiv.style.boxSizing = "border-box";
+      childDiv.style.border = "1px solid black";
+      childDiv.style.height = hightAndWidth + "px";
+      childDiv.style.width = hightAndWidth + "px";
+      childDiv.addEventListener("mouseover", () => {
+        childDiv.style.backgroundColor = "lightblue";
+      });
+      // console.log(childDiv);
+      parentDiv.appendChild(childDiv);
+    }
+    reset = true;
+  }
+}
+
+function resetGrid() {
+  const children = document.querySelectorAll(".child");
+  children.forEach((child) => {
+    child.remove();
+  });
+  reset = false;
+}
+button.addEventListener("click", () => {
+  if (reset == false) {
+    createGrid(inputPrompt());
+  } else {
+    resetGrid();
+    createGrid(inputPrompt());
+  }
+});
